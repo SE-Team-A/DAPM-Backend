9247aac55a1e2741871169849478917a4d369027
Veiglin <nicolaiveiglin@gmail.com>
2024-10-01 13:44:37 +0200

    add missing Dtos on clienApi

---
+++ DAPM/DAPM.ClientApi/Models/DTOs/LoginRequestDto.cs
@@ -0,0 +1,8 @@
+namespace DAPM.ClientApi.Models.DTOs
+{
+    public class LoginRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ DAPM/DAPM.ClientApi/Models/DTOs/LoginResponseDto.cs
@@ -0,0 +1,8 @@
+namespace DAPM.ClientApi.Models.DTOs
+{
+    public class LoginResponse
+    {
+        public string Token { get; set; }
+        public string Message { get; set; }
+    }
+}
