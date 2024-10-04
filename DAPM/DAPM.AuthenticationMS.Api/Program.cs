fe5a45934e00c14d316f9891b2d5c5cf9018cff6
Veiglin <nicolaiveiglin@gmail.com>
2024-10-04 10:32:38 +0200

    Refactor Program.cs to add AuthenticationService and IAuthenticationService

--- DAPM/DAPM.AuthenticationMS.Api/Program.cs
+++ DAPM/DAPM.AuthenticationMS.Api/Program.cs
@@ -1,6 +1,8 @@
 using RabbitMQLibrary.Implementation;
 using RabbitMQLibrary.Extensions;
 using DAPM.AuthenticationMS.Api.Consumers;
+using DAPM.AuthenticationMS.Api.Services;
+using DAPM.AuthenticationMS.Api.Services.Interfaces;
 using RabbitMQLibrary.Messages.Authentication;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.EntityFrameworkCore;
@@ -15,6 +17,8 @@ builder.Services.AddQueueing(new QueueingConfigurationSettings
     RabbitMqPassword = "guest",
     RabbitMqUsername = "guest"
 });
+
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
 
 // subscribe here to rabbitmq queues
 
